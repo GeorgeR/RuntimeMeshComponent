be4b9fad4b7e0e661d616bb5249920790e6c5d97
GeorgeR <dukecg@gmail.com>
2019-07-13 13:06:36 +1000

    a

--- Source/RuntimeMeshComponent/RuntimeMeshComponent.Build.cs
+++ Source/RuntimeMeshComponent/RuntimeMeshComponent.Build.cs
@@ -9,7 +9,6 @@ public class RuntimeMeshComponent : ModuleRules
     {
         PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-<<<<<<< HEAD
         if (Target.Version.MinorVersion <= 19)
         {
             PublicIncludePaths.AddRange(
@@ -22,33 +21,30 @@ public class RuntimeMeshComponent : ModuleRules
                     "RuntimeMeshComponent/Private"
                 });
         }
-        
-        PublicDependencyModuleNames.AddRange(
-            new string[] {
-                "Core"
-			});
-=======
-		PrivateIncludePaths.AddRange(new string[] { Path.Combine(ModuleDirectory, "Private") });
-		PublicIncludePaths.AddRange(new string[] { Path.Combine(ModuleDirectory, "Public") });
 
 		PublicDependencyModuleNames.AddRange(
             new string[]
             {
                 "Core",
-				// ... add other public dependencies that you statically link with here ...
-			}
-            );
-
->>>>>>> 422_ready
+			});
 
         PrivateDependencyModuleNames.AddRange(
             new string[] {
                 "CoreUObject",
                 "Engine",
                 "RenderCore",
-<<<<<<< HEAD
+
                 "RHI",
             });
+
+        if (Target.Version.MinorVersion >= 22)
+        {
+            PrivateDependencyModuleNames.AddRange(
+                new string[]
+                {
+                    "NavigationSystem"
+                });
+        }
 
         if (Target.Version.MinorVersion <= 21)
         {
@@ -58,24 +54,5 @@ public class RuntimeMeshComponent : ModuleRules
                     "ShaderCore",
                 });
         }
-=======
-#if !UE_4_22_OR_LATER
-				"ShaderCore",  // ShaderCore was Merged into RenderCore in 4.22
-#endif
-				"RHI",
-#if UE_4_20_OR_LATER
-				"NavigationSystem"
-#endif
-            }
-            );
-
-
-        DynamicallyLoadedModuleNames.AddRange(
-            new string[]
-            {
-				// ... add any modules that your module loads dynamically here ...
-			}
-            );
->>>>>>> 422_ready
     }
 }
